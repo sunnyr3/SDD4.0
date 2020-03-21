57061e68b125bdbb938298dddcaa8bbf394a17e4
asurashen <asurashen8@gmail.com>
2020-03-21 03:18:31 -0400

    try3

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './components/MainPage';
+import TutorialPage from'./components/MainPage';
 
 class AppRouter extends Component {
     render() {
