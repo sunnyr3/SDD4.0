bb60c084e3479fc0daf4a67723e72cb6e4027f41
asurashen <39765452+asurashen@users.noreply.github.com>
2020-03-28 13:33:42 -0400

    tutorialpage update folder sectio n

--- client/src/components/TutorialPage.js
+++ client/src/components/TutorialPage.js
@@ -2,17 +2,50 @@ import React, { Component } from 'react';
 import { Container} from 'reactstrap';
 import PageFrame from './PageFrame';
 
+import Fcards from './Fcards'
+
 import './TutorialPage.css';
 
 
 class TutorialPage extends Component {
+
+    super(props);
+    this.state = {
+       
+       folders: [
+        {id: 1, name: "number", cards: [{},{},{}]}
+        {id: 2, name: "character", cards: []}
+        {id: 3, name: "more", cards:[] }
+       ],
+    }
+
     render() {
       return(
         <PageFrame>
             <Container>
-                <div>
-                    <p> Welcome to TutorialPage</p>
+                //three folders - numbers, characters, more
+                <div className = "folders-preview">
+                    <div className = "folder1">
+                        <Fcards cards ={this.state.folders.cards} 
+                         name = {this.state.folders.name} />  
+                    </div>
+
+                    <div className = "folder2">
+                        <Fcards cards ={this.state.folders.cards} 
+                         name = {this.state.folders.name} />  
+                    </div>
+
+                    <div className = "folder3">
+                        <Fcards cards ={this.state.folders.cards} 
+                         name = {this.state.folders.name} />  
+                    </div>
                 </div>
+
+                //page ending
+                <div className = "page-foot">
+                 <strong>Welcome to TutorialPage!</strong>
+                </div>
+
             </Container>
         </PageFrame>
       );
