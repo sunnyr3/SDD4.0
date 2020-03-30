8d49481359c284d60182fa52466e6c6b3f8c3aa2
asurashen <39765452+asurashen@users.noreply.github.com>
2020-03-30 17:25:50 -0400

    Update TutorialPage.js
    code review

--- client/src/components/TutorialPage.js
+++ client/src/components/TutorialPage.js
@@ -3,7 +3,7 @@ import { Container} from 'reactstrap';
 import PageFrame from './PageFrame';
 import Fcards from './Fcards'
 import './TutorialPage.css';
-
+import axios from 'axios';
 
 class TutorialPage extends Component {
 
@@ -15,33 +15,23 @@ class TutorialPage extends Component {
 
             //sign language & explanation data
             
-            numbers: {'1': 'https://i.ibb.co/R40rKGC/1.jpg',
-                      '2': 'https://i.ibb.co/HNLRcDw/2.jpg',
-                      '3': 'https://i.ibb.co/W27Gr39/3.jpg',
-                      '4': 'https://i.ibb.co/r46sRWV/4.jpg',
-                      '5': 'https://i.ibb.co/zFknqPd/5.jpg',
-                      '6': 'https://i.ibb.co/CVpVSPq/6.jpg',
-                      '7': 'https://i.ibb.co/wC33gFc/7.jpg',
-                      '8': 'https://i.ibb.co/jv3Lkmp/8.jpg',
-                      '9': 'https://i.ibb.co/tcGzdYc/9.jpg'},
-            alphabets: {'a': 'https://i.ibb.co/TLkkghg/A.png','b': 'https://i.ibb.co/BNsLzvS/B.png',
-                        'c': 'https://i.ibb.co/fGKzr5F/C.png','d': 'https://i.ibb.co/G962j3F/D.png',
-                        'e': 'https://i.ibb.co/JKbBPSX/E.png','f': 'https://i.ibb.co/ZBCqD8V/F.png',
-                        'g': 'https://i.ibb.co/9Hjc9yx/G.png','h': 'https://i.ibb.co/CmPHBzP/H.png',
-                        'i': 'https://i.ibb.co/hK11HvT/I.png','j': 'https://i.ibb.co/nLHs7dt/J.png',
-                        'k': 'https://i.ibb.co/x2RGWxR/K.png','l': 'https://i.ibb.co/QPR3fD4/L.png',
-                        'm': 'https://i.ibb.co/Z14MyPC/M.png','n': 'https://i.ibb.co/HYktXw2/N.png',
-                        'o': 'https://i.ibb.co/2d49M00/O.png','p': 'https://i.ibb.co/dPr2Hf6/P.png',
-                        'q': 'https://i.ibb.co/RSK3GP9/Q.png','r': 'https://i.ibb.co/VDZpskk/R.png',
-                        's': 'https://i.ibb.co/C55gkCQ/S.png','t': 'https://i.ibb.co/b1pM20F/T.png',
-                        'u': 'https://i.ibb.co/h2c83Dd/U.png','v': 'https://i.ibb.co/0hMMH4h/V.png',
-                        'w': 'https://i.ibb.co/2PPf224/W.png','x': 'https://i.ibb.co/dcQ32kj/X.png',
-                        'y': 'https://i.ibb.co/DC2G3HY/Y.png','z': 'https://i.ibb.co/rxhfVdt/Z.png'}  
+            numbers: undefiend,
+            
+            alphabets: undefined
                   
         };
     }
 
-
+//function to access data(maps) from backend
+componentDidMount() {
+        axios.get("http://localhost:8000/tutorial/").then((res) => {
+            console.log(res.data);
+            this.setState({
+                numbers: res.data.numbers,
+                alphabets: res.data.alphabets
+            });
+        });
+}
 
 //function to get the size of a folder(map)
     getmapsize = (m) => {
